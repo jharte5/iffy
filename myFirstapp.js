2f83d3a22028ed497462d78aa999301ae2b92612
jharte5 <jonathan.harte@codeimmersives.com>
2019-10-03 14:01:30 -0400

    initial commit

---
+++ myFirstapp.js
@@ -0,0 +1,11 @@
+// i want to make an app where we can calculate a taxi ride.
+//  rides between 6 am and 6pm initial fare $3.50 and adds $1.50 every 5 blocks.
+// rides between 6 pm and 6 am initial fare is $5.00 and adds $2.00 every 5 blocks
+
+
+// const amRide = process.argv[];
+// const pmRide = process.argv[];
+
+function checkRideCost (amRide, pmRide) {
+    
+}
